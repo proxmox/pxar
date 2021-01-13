a5b958d10c63e4c27b69f821725d60e9e6d35a2e
Wolfgang Bumiller <w.bumiller@proxmox.com>
2021-01-13 09:59:58 +0100

    fix example
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- examples/apxar.rs
+++ examples/apxar.rs
@@ -12,21 +12,8 @@ async fn main() {
     let mut reader = Decoder::from_tokio(file)
         .await
         .expect("failed to open pxar archive contents");
-    let mut i = 0;
+
     while let Some(entry) = reader.next().await {
-        println!("{:#?}", entry.expect("failed to parse entry").path());
-        i += 1;
-        if i == 2 {
-            break;
-        }
-    }
-
-    // Use a Stream for the remaining entries:
-    use futures::stream::StreamExt;
-
-    let mut stream = reader.into_stream();
-
-    while let Some(entry) = stream.next().await {
         println!("{:#?}", entry.expect("failed to parse entry").path());
     }
 }
